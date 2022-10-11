1127b11ce6ff3c3a0e63925a8049863721b904d3
koyu <me@koyu.space>
2022-10-11 05:41:12 +0200

    Add date, image, video and Pleroma support

--- src/ts/ui/timeline.ts
+++ src/ts/ui/timeline.ts
@@ -6,7 +6,25 @@ export function renderTimeline(selector, data) {
         let status = "<div class=\"card bg-dark status\">";
         let display_name = element.account.display_name;
         status += `<p><b class="display-name" id="dp-${element.account.id}">${display_name}</b></p>`;
-        status += element.content;
+        status += `<p>${element.content}</p>`;
+        if (element.media_attachments.length > 0) {
+            status += "<p>";
+            element.media_attachments.forEach((attachment) => {
+                if (attachment.type === "image") {
+                    status += `<a href="${attachment.url}" target="_blank"><img src=${attachment.preview_url} width="300" alt="${attachment.description}"></a> `;
+                }
+                if (attachment.type === "video") {
+                    status += `<video src=${attachment.url} width="300" alt="${attachment.description}" controls></video> `;
+                }
+            });
+            status += "</p>";
+        }
+        let statusdate = new Date(Date.parse(element.created_at)).toLocaleString();
+        let statusurl = element.url;
+        if (element.url === "") {
+            statusurl = element.uri;
+        }
+        status += `<p><a href="${statusurl}" target="_blank">${statusdate}</p>`
         status += "</div>";
         $(selector).append(status);
         if (element.account.emojis.length > 0) {
