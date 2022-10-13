482c52f77b42ee7092882e713c17592fe7b16d0e
koyu <me@koyu.space>
2022-10-13 18:38:18 +0200

    Properly handle boosted content

--- src/ts/ui/timeline.ts
+++ src/ts/ui/timeline.ts
@@ -9,6 +9,7 @@ const iconFav = '<svg viewBox="0 0 16 16" astro-icon="bi:star"><path fill="curre
 const iconUnfav = '<svg viewBox="0 0 16 16" astro-icon="bi:star-fill"><path fill="currentColor" d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path></svg>';
 const iconBoost = '<svg viewBox="0 0 16 16" class="icon" astro-icon="bi:arrow-repeat"><g fill="currentColor"><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"></path><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"></path></g></svg>';
 const iconReply = '<svg viewBox="0 0 16 16" class="icon" astro-icon="bi:arrow-90deg-left"><path fill="currentColor" fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4z"></path></svg>';
+const iconCopy = '<svg viewBox="0 0 16 16" class="icon" astro-icon="bi:file-earmark-fill"><path fill="currentColor" d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm5.5 1.5v2a1 1 0 0 0 1 1h2l-3-3z"></path></svg>';
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -16,6 +17,7 @@ function capitalizeFirstLetter(string) {
 
 export function renderTimeline(data, threadmode=false, ispost=false) {
     let statuses = [];
+    console.log(data);
     data.forEach(element => {
         let status = "";
         if (ispost) {
@@ -41,9 +43,10 @@ export function renderTimeline(data, threadmode=false, ispost=false) {
                     });
                 }
                 status += `<p><b>${iconBoost} <a href="/user?id=${element.reblog.account.id}"><img src="${element.reblog.account.avatar}" class="avatar" width="16" height="16" alt="${element.reblog.display_name}'s Avatar"></a> Boosted ${reblog_display_name}</b></p><br>`;
+                content = element.reblog.content;
             }
-            content += element.content;
             if (element.reblog === null) {
+                content = element.content;
                 if (element.emojis.length > 0) {
                     element.emojis.forEach(pc_emoji => {
                         content = content.replaceAll(`:${pc_emoji.shortcode}:`, `<img src="${pc_emoji.url}" alt="Emoji ${pc_emoji.shortcode}" class="emoji">`);
@@ -75,9 +78,10 @@ export function renderTimeline(data, threadmode=false, ispost=false) {
                     });
                 }
                 status += `<p><b>${iconBoost} <a href="/user?id=${element.reblog.account.id}"><img src="${element.reblog.account.avatar}" class="avatar" width="16" height="16" alt="${element.reblog.display_name}'s Avatar"></a> Boosted ${reblog_display_name}</b></p><br>`;
+                content = element.reblog.content;
             }
-            content += element.content;
             if (element.reblog === null) {
+                content = element.content;
                 if (element.emojis.length > 0) {
                     element.emojis.forEach(pc_emoji => {
                         content = content.replaceAll(`:${pc_emoji.shortcode}:`, `<img src="${pc_emoji.url}" alt="Emoji ${pc_emoji.shortcode}" class="emoji">`);
@@ -162,9 +166,9 @@ export function renderTimeline(data, threadmode=false, ispost=false) {
         status += "</p><br>";
         let statusdate = new Date(Date.parse(element.created_at)).toLocaleString();
         if (element.reblog === null) {
-            status += `<p><a href="/thread?id=${element.id}">${statusdate}</a> | ${capitalizeFirstLetter(element.visibility)}</p>`;
+            status += `<p><a href="/thread?id=${element.id}">${statusdate}</a> | ${capitalizeFirstLetter(element.visibility)} | <a href="javascript: navigator.clipboard.writeText('${element.url}')" class="text-white" style="text-decoration:none;">${iconCopy} Copy link</a></p>`;
         } else {
-            status += `<p><a href="/thread?id=${element.reblog.id}">${statusdate}</a> | ${capitalizeFirstLetter(element.visibility)}</p>`;
+            status += `<p><a href="/thread?id=${element.reblog.id}">${statusdate}</a> | ${capitalizeFirstLetter(element.visibility)} | <a href="javascript: navigator.clipboard.writeText('${element.reblog.url}')" class="text-white" style="text-decoration:none;">${iconCopy} Copy link</a></p>`;
         }
         status += "</div>";
         localStorage.setItem("last-element", element.id);
