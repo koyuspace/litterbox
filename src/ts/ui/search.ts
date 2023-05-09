bf63471e9c4430e5ba0823d719a599496efdce98
koyu <me@koyu.space>
2023-05-09 13:17:45 +0200

    Redirect when there's only one object in search

--- src/ts/ui/search.ts
+++ src/ts/ui/search.ts
@@ -17,25 +17,33 @@ export function loadSearch(query) {
     if (data.accounts.length === 0) {
       html += "<p>No accounts found.</p>";
     } else {
-      data.accounts.forEach((account) => {
-        let display_name = account.display_name;
-        if (account.emojis.length > 0) {
-          account.emojis.forEach((dp_emoji) => {
-            display_name = display_name.replaceAll(
-              `:${dp_emoji.shortcode}:`,
-              `<img src="${dp_emoji.url}" alt=":${dp_emoji.shortcode}:" class="emoji">`
-            );
-          });
-        }
-        html += `<p><a href="/user?id=${account.id}"><img src="${account.avatar}" class="avatar" width="16" height="16" alt="${account.display_name}'s Avatar"> `;
-        html += `${display_name}</a></p>`;
-      });
+      if (data.accounts.length === 1) {
+        location.replace("/user?id="+data.accounts[0].id);
+      } else {
+        data.accounts.forEach((account) => {
+          let display_name = account.display_name;
+          if (account.emojis.length > 0) {
+            account.emojis.forEach((dp_emoji) => {
+              display_name = display_name.replaceAll(
+                `:${dp_emoji.shortcode}:`,
+                `<img src="${dp_emoji.url}" alt=":${dp_emoji.shortcode}:" class="emoji">`
+              );
+            });
+          }
+          html += `<p><a href="/user?id=${account.id}"><img src="${account.avatar}" class="avatar" width="16" height="16" alt="${account.display_name}'s Avatar"> `;
+          html += `${display_name}</a></p>`;
+        });
+      }
     }
     html += "<br><h1>Statuses</h1>";
     if (data.statuses.length === 0) {
       html += "<p>No statuses found.</p>";
     } else {
-      html += renderTimeline(data.statuses);
+      if (data.statuses.length === 1) {
+        location.replace("/thread?id="+data.statuses[0].id);
+      } else {
+        html += renderTimeline(data.statuses);
+      }
     }
     html += "<br><h1>Hashtags</h1>";
     if (data.hashtags.length === 0) {
