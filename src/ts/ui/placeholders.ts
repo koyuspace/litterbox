978a82dee89c75697670d1056b754544e5cf5aaf
koyu <me@koyu.space>
2023-05-09 13:24:33 +0200

    Proper profile link again

--- src/ts/ui/placeholders.ts
+++ src/ts/ui/placeholders.ts
@@ -4,36 +4,27 @@ import { api } from "../api/request";
 let username = "%username%";
 let userpic = "";
 let userid = "";
-let acct = "";
 let instance = localStorage.getItem("instance");
 
-api(
-  instance,
-  "/api/v1/accounts/verify_credentials",
-  true,
-  "GET",
-  {},
-  localStorage.getItem("token")
-).then((data) => {
-  username = data.username;
-  userpic = data.avatar;
-  userid = data.id;
-  acct = data.acct;
-  localStorage.setItem("userid", data.id);
+api(instance, "/api/v1/accounts/verify_credentials", true, "GET", {}, localStorage.getItem("token")).then((data) => {
+    username = data.username;
+    userpic = data.avatar;
+    userid = data.id;
+    localStorage.setItem("userid", data.id);
 });
 
 export function initializePlaceholders() {
-  window.setInterval(() => {
-    $(".username").html(username);
-    $(".edit-profile").attr("href", `https://${instance}/settings/profile`);
-  });
+    window.setInterval(() => {
+        $(".username").html(username);
+        $(".edit-profile").attr("href", `https://${instance}/settings/profile`);
+    });
 }
 
 $(document).ready(() => {
-  $(".userpic").attr("src", userpic);
-  $(".userlink").attr("href", `https://${instance}/@${acct}`);
-  window.setInterval(() => {
     $(".userpic").attr("src", userpic);
-    $(".userlink").attr("href", `https://${instance}/@${acct}`);
-  }, 1000);
+    $(".userlink").attr("href", `/user?id=${userid}`);
+    window.setInterval(() => {
+        $(".userpic").attr("src", userpic);
+        $(".userlink").attr("href", `/user?id=${userid}`);
+    }, 1000);
 });
