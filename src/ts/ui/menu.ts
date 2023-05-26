9f0c84b0c0d88df74601e340a6d15c6d1f50cb7e
koyu <me@koyu.space>
2023-05-26 21:43:27 +0200

    Warn about old server versions

--- src/ts/ui/menu.ts
+++ src/ts/ui/menu.ts
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import { api } from "../api/request";
 import { Md5 } from "ts-md5";
+import { semver } from "semver";
 
 const domain = location.href.split("/")[2];
 let protocol = "https://";
@@ -31,6 +32,9 @@ window.setInterval(loadNotificationLength, 5000);
 api(localStorage.getItem("instance"), "/api/v1/instance", true, "GET", {}, localStorage.getItem("token")).then((data) => {
     $(".navbar-brand").html(`<img src="${data.thumbnail}" id="logo" alt="Logo"> ${data.title}`);
     document.title = data.title;
+    if (semver.lt(data.version, "0.9.0")) {
+        $("#version-warning").removeAttr("style");
+    }
 });
 
 let fediid = localStorage.getItem("acct");
