3acb1c2743c11b45dee35ac2bc8b67eccea834a2
koyu <me@koyu.space>
2024-10-23 14:01:20 +0200

    Instances are now servers

--- src/ts/timelines/local.ts
+++ src/ts/timelines/local.ts
@@ -5,7 +5,7 @@ import { renderTimeline } from "../ui/timeline";
 export function loadLocal() {
     api(localStorage.getItem("instance"), "/api/v1/timelines/public?local=true", true, "GET", {}, localStorage.getItem("token")).then((data) => {
         if (data.length === 0) {
-            $("#local").html("<div class='alert alert-warning'>No posts found. Invite some people to your instance or post publicly and posts will show up here.</div>");
+            $("#local").html("<div class='alert alert-warning'>No posts found. Invite some people to your server or post publicly and posts will show up here.</div>");
         } else {
             $("#local").html(renderTimeline(data));
         }
